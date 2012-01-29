85c33c558399a81add6a3f82dcb0339332295a40
avmorozov <morozov@ilabsltd.com>
2012-01-29 12:49:40 +0400

    Fixed last FXCop violations

--- src/AV.Models/Repositary/FakeRepositaryException.cs
+++ src/AV.Models/Repositary/FakeRepositaryException.cs
@@ -5,14 +5,24 @@
 // -----------------------------------------------------------------------
 
 using System;
+using System.Runtime.Serialization;
 
 namespace AV.Models.Repositary
 {
     /// <summary>
     ///   Custom exception for fake repositary
     /// </summary>
+    [Serializable]
     public class FakeRepositaryException : Exception
     {
+        public FakeRepositaryException() {}
+
         public FakeRepositaryException(string message) : base(message) {}
+
+        public FakeRepositaryException(string message, Exception innerException) :
+            base(message, innerException) {}
+
+        protected FakeRepositaryException(SerializationInfo info,
+                                          StreamingContext context) : base(info, context) {}
     }
 }
